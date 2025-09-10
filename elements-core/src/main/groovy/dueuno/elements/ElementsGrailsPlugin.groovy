b2dcdd690d8a97eec84a03a338e21946eadbe260
g.sartori <g.sartori@gmail.com>
2025-09-10 16:05:52 +0200

    3.0.0-RC1

--- elements-core/src/main/groovy/dueuno/elements/ElementsGrailsPlugin.groovy
+++ elements-core/src/main/groovy/dueuno/elements/ElementsGrailsPlugin.groovy
@@ -34,9 +34,9 @@ class ElementsGrailsPlugin extends Plugin {
 
     static final String NAME = 'elements-core'
 
-    def version = '2.x-SNAPSHOT'
+    def version = '3.0.0-RC1'
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = '6.2.0 > *'
+    def grailsVersion = '7.0.0 > *'
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         'grails-app/views/error.gsp'
